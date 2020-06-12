cb40a584cf886591eb269b7415532130f258ee31
EliasKaiba <elias_ibarra95@hotmailcom>
2020-06-12 15:52:21 -0600

    create page in pageDesigner and create for practice

--- controllers/controlerpractice.js
+++ controllers/controlerpractice.js
@@ -1,22 +1,11 @@
 'use strict';
 
 var server = require('server');
-var cache = require('*/cartridge/scripts/middleware/cache');
-var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
-var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+server.get('Show', function (req, res, next) {
+    
+    var formtest = server.forms.getForm('testform');
 
-server.get('Show', consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
-    var Site = require('dw/system/Site');
-    var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
-
-    pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
-    res.render('/test/templateexample');
-    next();
-}, pageMetaData.computedPageMetaData);
-
-server.get('ErrorNotFound', function (req, res, next) {
-    res.setStatusCode(404);
-    res.render('error/notFound');
+    res.render('/test/templateexample',{form: formtest});
     next();
 });
 
